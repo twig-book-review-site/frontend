3221fdc42670e12b9cff015ea6498d649606557b
saanglee <sangllee20@gmail.com>
2023-08-29 22:48:47 +0900

    button 진행 중

---
+++ src/components/elements/button.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import type { VariantProps } from 'class-variance-authority';
+import { cva } from 'class-variance-authority';
+
+const button = cva(['rounded-[100px]', 'border', 'rounded'], {
+  variants: {
+    intent: {
+      primary: ['bg-black', 'text-white', 'hover:bg-blue-600'],
+      secondary: ['bg-white', 'text-gray-800', 'border-gray-400', 'hover:bg-gray-100'],
+    },
+    size: {
+      xlarge: ['h-[52px]', 'text-base', 'px-5', 'py-4'],
+      small: ['text-sm', 'py-1', 'py-2'],
+      medium: ['text-base', 'py-2', 'py-4'],
+    },
+  },
+  // compoundVariants: [
+  // {
+  // primary이고 medium일 때 upppercase 적용
+  // intent: 'primary',
+  // size: 'medium',
+  // class: 'upppercase',
+  // },
+  // ],
+  defaultVariants: {
+    intent: 'primary',
+    size: 'medium',
+  },
+});
+
+export interface ButtonVariantProps extends VariantProps<typeof button> {
+  children: React.ReactNode;
+}
+
+export default function Button({ intent, size, children }: ButtonVariantProps) {
+  return <button className={button({ intent, size })}>{children}</button>;
+}
