b369a0202abab21f2b1fe3ced4c4cc61ff90685b
saanglee <sangllee20@gmail.com>
2023-08-29 22:48:35 +0900

    colors 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -10,11 +10,45 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: {
+          600: '#adb902',
+          500: '#e4f222',
+          200: '#f5ff78',
+          100: '#feffe1',
+        },
+        secondary: '#1f1f1f',
+        negative: {
+          DEFAULT: '#d82c0d',
+          light: '#fff4f4',
+        },
+        positive: {
+          DEFAULT: '#2c6ecb',
+          light: '#f2f7fe',
+        },
+        gray: {
+          50: '#f8f9fa',
+          100: '#f1f3f5',
+          200: '#e9ecef',
+          300: '#e9ecef',
+          400: '#ccd2d8',
+          500: '#aeb5ba',
+          600: '#888e94',
+          650: '#646a70',
+          700: '#484f54',
+          750: '#3a3e43',
+          800: '#32363a',
+          850: '#28262b',
+          900: '#1f2022',
+          950: '#171313',
+        },
+        kakao: '#fee500',
       },
     },
   },
+
   plugins: [],
-}
-export default config
+};
+export default config;
