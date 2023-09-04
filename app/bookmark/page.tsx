0e74f39a72bc08f0ddddc471b1663e73bdb5e7c6
sangllee <sangllee20@gmail.com>
2023-09-04 17:48:07 +0900

    button 컴포넌트 진행 중

--- app/bookmark/page.tsx
+++ app/bookmark/page.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Button from '@/app/components/elements/button';
+import Textarea from '../components/elements/textarea';
 
 const Bookmark = () => {
   return (
     <div>
       <h2>Bookmark</h2>
       <Button intent='primary' size='medium'>
-        Button
+        primary
       </Button>
-      <hr />
+      <Button intent='secondary' size='medium'>
+        secondary
+      </Button>
+      <Button size='medium' disabled>
+        disabled
+      </Button>
+      <br />
+      <Textarea />
     </div>
   );
 };
--- app/components/elements/button.tsx
+++ app/components/elements/button.tsx
@@ -2,21 +2,26 @@ import * as React from 'react';
 import type { VariantProps } from 'class-variance-authority';
 import { cva } from 'class-variance-authority';
 
-const button = cva(['rounded-[100px]', 'border', 'rounded'], {
+const button = cva(['rounded-[10px]', 'border'], {
   variants: {
     intent: {
       primary: ['bg-black', 'text-white', 'hover:bg-blue-600'],
       secondary: ['bg-white', 'text-gray-800', 'border-gray-400', 'hover:bg-gray-100'],
     },
     size: {
-      xlarge: ['h-[52px]', 'text-base', 'px-5', 'py-4'],
+      large: ['h-[52px]', 'text-base', 'px-5', 'py-4'],
+      medium: ['text-sm', 'py-2', 'px-3'],
       small: ['text-sm', 'py-1', 'py-2'],
-      medium: ['text-base', 'py-2', 'py-4'],
+    },
+    disabled: {
+      true: ['cursor-not-allowed', 'bg-gray-200', 'text-gray-500', 'hover:bg-gray-200'],
     },
   },
+
   defaultVariants: {
     intent: 'primary',
     size: 'medium',
+    disabled: false,
   },
 });
 
@@ -24,6 +29,10 @@ export interface ButtonVariantProps extends VariantProps<typeof button> {
   children: React.ReactNode;
 }
 
-export default function Button({ intent, size, children }: ButtonVariantProps) {
-  return <button className={button({ intent, size })}>{children}</button>;
+export default function Button({ intent, size, disabled, children }: ButtonVariantProps) {
+  return (
+    <button className={button({ intent, size, disabled })} disabled>
+      {children}
+    </button>
+  );
 }
