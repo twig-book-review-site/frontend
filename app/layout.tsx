eead092dd3e1ae960f1a294d1ad456af2116f663
sangllee <sangllee20@gmail.com>
2023-10-03 18:24:17 +0900

    로그인 & 책장 layout 분리하여 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,12 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        <div className='flex'>
-          <Navigation />
-          {children}
-        </div>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
