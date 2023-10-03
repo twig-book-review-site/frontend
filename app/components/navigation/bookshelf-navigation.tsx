43f7053e2ba1fa4c68b529de07b9460044cbe23e
sangllee <sangllee20@gmail.com>
2023-10-03 18:34:41 +0900

    내책장 (책목록) navigation에 dummy data 반영

--- app/components/navigation/bookshelf-navigation.tsx
+++ app/components/navigation/bookshelf-navigation.tsx
@@ -1,20 +1,25 @@
 import Link from 'next/link';
 import React from 'react';
+import BookCard from '../bookshelf/book-card';
+import { DUMMY_BOOKS } from '@/DUMMY_DATA';
 
 const BookshelfNavigation = () => {
   return (
     <div className='border-solid border-2 border-sky-500 h-screen w-72'>
+      <h1>나의 책장</h1>
+      <div className='flex justify-between'>
+        <span>전체{DUMMY_BOOKS.length}권</span>
+        <span>책 추가</span> {/* 책 추가 버튼 */}
+      </div>
       <nav>
         <ul>
-          <li>
-            <Link href='/bookshelf'>책</Link>
-          </li>
-          <li>
-            <Link href='/bookshelf'>책</Link>
-          </li>
-          <li>
-            <Link href='/bookshelf'>책</Link>
-          </li>
+          {DUMMY_BOOKS.map((book) => (
+            <li key={book.title}>
+              <Link href={`/bookshelf/${book.id}`}>
+                <BookCard title={book.title} author={book.author} sentences={book.sentences} ideas={book.ideas} />
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
