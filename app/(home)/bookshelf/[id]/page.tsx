4836fdbcd37da729188d6d5770485eedbd02651c
sangllee <sangllee20@gmail.com>
2023-10-16 21:53:31 +0900

    내책장 페이지 - 헤더 생성 및 문장카드 적용

--- app/(home)/bookshelf/[id]/page.tsx
+++ app/(home)/bookshelf/[id]/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { DUMMY_MY_BOOKS } from '@/DUMMY_DATA';
 import { notFound } from 'next/navigation';
+import Display from '@/app/components/typography/display';
+import SentenceCard from '@/app/components/bookshelf/sentence-card';
 
 interface SentenceCard {
   id: number;
@@ -8,7 +10,7 @@ interface SentenceCard {
   created: string;
   sentence: string;
   ideas: string[];
-  bookmarked: boolean;
+  isBookmarked: boolean;
 }
 
 export const generateStaticParams = async () => {
@@ -23,19 +25,33 @@ const Page = ({ params }: { params: { id: string } }) => {
   if (!book) return notFound();
 
   return (
-    <div>
-      <div>{book.title}</div>
-      <div>{book.author}</div>
-      <div>
+    <div className='w-full'>
+      {/* Header */}
+      <div className='h-20'>
+        <div className='w-full h-20 absolute bg-white opacity-50' />
+        <div className='w-full h-20 flex items-center px-3 absolute'>
+          <h1 className='font-batang text-3xl'>
+            {/* <Display size='large'>{book.title}</Display> */}
+            {book.title}
+          </h1>
+          <h2 className='font-batang text-xl pl-3'>
+            {/* <Display size='small'> {book.author}</Display> */}
+            {book.author}
+          </h2>
+        </div>
+      </div>
+      {/* Sentences */}
+      <div className='p-3'>
         {book?.sentence_cards.map((card: SentenceCard) => (
-          <div key={card.created}>
-            <div>{card.created}</div>
-            <div>{card.bookmarked}</div>
-            <div>{card.page}</div>
-            <div>{card.sentence}</div>
-            <div>(ideas: {card.ideas})</div>
-            <div>----------------------------------------------</div>
-          </div>
+          <SentenceCard
+            key={card.created}
+            id={card.id}
+            page={card.page}
+            created={card.created}
+            sentence={card.sentence}
+            ideas={card.ideas}
+            isBookmarked={card.isBookmarked}
+          />
         ))}
       </div>
     </div>
