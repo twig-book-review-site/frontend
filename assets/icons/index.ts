f961d33987b554ea129190c8b15dca170d03533b
sangllee <sangllee20@gmail.com>
2023-10-16 21:51:26 +0900

    아이콘 추가

---
+++ assets/icons/index.ts
@@ -0,0 +1,8 @@
+// export * as BlueSquarePath from "./logo-square-block.svg";
+// export * as LoadingSpinner from "./loading-spinner.svg";
+
+export * as BookmarkIcon from './bookmark-simple.svg';
+export * as DownRightArrowIcon from './down-right-arrow.svg';
+export * as MoreIcon from './more.svg';
+export * as BookshelfIcon from './bookshelf.svg';
+export * as UserIcon from './user.svg';
