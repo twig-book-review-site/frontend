a015fb567336442690bc26dd53f6d990b204ab14
sangllee <sangllee20@gmail.com>
2023-10-19 10:52:36 +0900

    navigation 수정 진행 중

--- app/components/navigation/navigation.tsx
+++ app/components/navigation/navigation.tsx
@@ -5,7 +5,8 @@ import Display from '../typography/display';
 
 const Navigation = () => {
   return (
-    <div className='bg-tertiary-300 border-r border-gray-300 h-screen lg:w-48 w-14'>
+    <div className='bg-tertiary-300 border-r border-gray-300 h-screen lg:w-40 md:w-16 w-12'>
+      {/* <div className='border-r border-gray-300 h-screen lg:bg-blue-300 md:bg-yellow-400 bg-red-400 w-13'> */}
       <div>
         <Link href='/'>
           <Display>Twig</Display>
