a0c21889ba92f692c13bb8919bb8686b5b47ad5f
sangllee <sangllee20@gmail.com>
2023-10-19 10:52:27 +0900

    bookshelf navigation 수정 진행 중

--- app/components/navigation/bookshelf-navigation.tsx
+++ app/components/navigation/bookshelf-navigation.tsx
@@ -6,7 +6,7 @@ import Display from '../typography/display';
 
 const BookshelfNavigation = () => {
   return (
-    <div className='h-screen lg:w-72 w-22 border-r border-gray-300 bg-tertiary-200'>
+    <div className='h-screen xl:w-72 lg:w-64 w-[70px] border-r border-gray-300 bg-tertiary-200'>
       <h1 className='px-2 py-3 hidden lg:block'>
         <Display size='small'>나의 책장</Display>
       </h1>
@@ -19,14 +19,7 @@ const BookshelfNavigation = () => {
           {DUMMY_BOOKS.map((book) => (
             <li key={book.title}>
               <Link href={`/bookshelf/${book.id}`}>
-                <BookCard
-                  id={book.id}
-                  title={book.title}
-                  author={book.author}
-                  sentences={book.sentences}
-                  ideas={book.ideas}
-                  // selectedBook={selectedBook}
-                />
+                <BookCard id={book.id} title={book.title} author={book.author} sentences={book.sentences} ideas={book.ideas} />
               </Link>
             </li>
           ))}
