584ad0c91c094518f1b88d2377757b7cae344170
sangllee <sangllee20@gmail.com>
2023-10-19 14:08:40 +0900

    book-card breakpoint, width 수정

--- app/components/bookshelf/book-card.tsx
+++ app/components/bookshelf/book-card.tsx
@@ -23,15 +23,10 @@ const BookCard = ({ title, author, sentences, ideas, id }: BookCardProps) => {
       ${page === id ? 'bg-tertiary-300 border-r-2 border-black' : ''}
       `}
     >
-      <div className='mr-4 drop-shadow-md'>
-        <Image
-          src='https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9791157326044.jpg'
-          alt={title}
-          width={50}
-          height={50}
-        />
+      <div className='w-[51px] drop-shadow-md'>
+        <Image src='https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9791157326044.jpg' alt={title} width={50} height={50} />
       </div>
-      <div className='hidden lg:block'>
+      <div className='hidden md:block'>
         <div>{title}</div>
         <div className='text-xs text-gray-500 pt-1'>{author}</div>
         <div className='text-xs text-gray-650 pt-2'>
