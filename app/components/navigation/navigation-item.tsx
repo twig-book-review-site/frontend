c5a54e12d1ab9a8e260467d1644f764c326720bf
sangllee <sangllee20@gmail.com>
2023-10-25 16:12:47 +0900

    navigation icon추가

--- app/components/navigation/navigation-item.tsx
+++ app/components/navigation/navigation-item.tsx
@@ -3,16 +3,25 @@ import Image from 'next/image';
 import { BookmarkIcon, UserIcon, BookshelfIcon } from '@/assets/icons';
 
 interface NavigationItemProps {
-  name: string;
+  name: '책장' | '북마크' | '프로필';
 }
 
+const navigationIcons = {
+  책장: 'bookshelf',
+  북마크: 'bookmark-simple',
+  프로필: 'user',
+};
+
 const NavigationItem = ({ name }: NavigationItemProps) => {
+  console.log(name);
+
   return (
     <li className='p-3 hover:bg-tertiary-400 font-gothic text-base flex'>
       <span>
-        <Image src={BookshelfIcon} alt='bookshelf' />
+        {/* navigationIcons[name] */}
+        <Image src={`/${navigationIcons[name]}.svg`} alt='bookshelf' width={30} height={40} />
       </span>
-      <span className='xl:inline hidden ml-1'>{name}</span>
+      <span className='xl:inline hidden ml-1 font-batang'>{name}</span>
     </li>
   );
 };
