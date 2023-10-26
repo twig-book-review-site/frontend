11ee02024050483fc6e1f97cf2504e27ea401998
sangllee <sangllee20@gmail.com>
2023-10-26 19:24:28 +0900

    button 컴포넌트 수정

--- app/components/elements/button.tsx
+++ app/components/elements/button.tsx
@@ -2,17 +2,17 @@ import * as React from 'react';
 import type { VariantProps } from 'class-variance-authority';
 import { cva } from 'class-variance-authority';
 
-const button = cva(['cursor-pointer, font-gothic'], {
+const button = cva(['cursor-pointer', 'font-gothic'], {
   variants: {
     intent: {
-      primary: ['bg-primary-200', 'text-gray-800', 'hover:bg-primary-500'],
-      secondary: ['bg-tertiary-500', 'text-white', 'hover:bg-tertiary-600'],
-      // disabled: ['cursor-not-allowed', 'bg-gray-200', 'text-gray-500', 'hover:bg-gray-200'],
+      cta: ['bg-primary-200', 'text-gray-800', 'hover:bg-primary-500'],
+      primary: ['bg-tertiary-500', 'text-white', 'hover:bg-tertiary-600'],
+      secondary: ['text-gray-800', 'hover:bg-tertiary-300'],
     },
     size: {
       large: ['h-[52px]', 'text-base', 'px-5', 'py-4'],
       medium: ['text-sm', 'py-2', 'px-3'],
-      small: ['text-sm', 'py-1', 'py-2'],
+      small: ['text-xs', 'py-1', 'px-2'],
     },
     disabled: {
       true: ['cursor-not-allowed', 'bg-gray-200', 'text-gray-500', 'hover:bg-gray-200'],
